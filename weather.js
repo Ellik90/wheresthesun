bdbe644d731e63a60c4ec3be118cfac6b8024575
Elina Kerola <elinak90@icloud.com>
2023-07-18 00:57:10 +0200

    api key is not working with this invisible

--- weather.js
+++ weather.js
@@ -1,13 +1,48 @@
+
+require('dotenv').config();
+
 const apiKey = process.env.API_KEY;
 
-const city = 'Stockholm'; // Byt ut mot den stad du vill ha väderdata för
 
-fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`)
-  .then(response => response.json())
-  .then(data => {
-    // Här får du svaret från API:et i "data"-objektet
-    console.log(data);
-  })
-  .catch(error => {
-    console.error('Ett fel inträffade:', error);
-  });
+
+fetchWeatherData('Stockholm');
+
+const searchForm = document.getElementById('search-form');
+const cityInput = document.getElementById('city-input');
+const weatherInfoDiv = document.getElementById('weather-info');
+
+searchForm.addEventListener('submit', function (event) {
+  event.preventDefault();
+  const city = cityInput.value;
+  fetchWeatherData(city);
+});
+
+function fetchWeatherData(city) {
+  fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`)
+    .then(response => response.json())
+    .then(data => {
+      displayWeatherInfo(data);
+    })
+    .catch(error => {
+      console.error('Ett fel inträffade:', error);
+      displayErrorMessage();
+    });
+}
+
+function displayWeatherInfo(data) {
+  const cityName = data.name;
+  const temperature = data.main.temp;
+  const weatherDescription = data.weather[0].description;
+  const windSpeed = data.wind.speed;
+
+  weatherInfoDiv.innerHTML = `
+    <h2>${cityName}</h2>
+    <p>Temperature: ${temperature} K</p>
+    <p>Weather: ${weatherDescription}</p>
+    <p>Wind Speed: ${windSpeed} m/s</p>
+  `;
+}
+
+function displayErrorMessage() {
+  weatherInfoDiv.innerHTML = `<p>Hittade inte staden. Försök igen.</p>`;
+}
